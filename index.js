74419d75e9dc327d805a73168edc72a81f1da090
K-ManojBabu <100508188+K-ManojBabu@users.noreply.github.com>
2024-12-30 10:56:08 +0530

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// JavaScript functionality for the website
-
-// Alert button functionality
 document.getElementById('alertButton').addEventListener('click', function() {
   alert('Welcome to Angaluru Village, Andhra Pradesh!');
 });
